9758d580e9d33828f67f3c72ec5de462f3d1189f
Anthony Bobsin <anthony.bobsin.dev@gmail.com>
2019-11-20 19:11:07 -0500

    add rpc namespace

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 curl --request POST \
-  --url http://localhost:3000/demo.v1.LocationRetrieval/GetLocations \
+  --url http://localhost:3000/rpc/demo.v1.LocationRetrieval/GetLocations \
   --header 'Content-Type: application/json' \
   --data '{"warehouse_id": 1}
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ require 'rpc/demo/v1/locations_pb'
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  service = Demo::V1::LocationRetrievalService.new(LocationRetrievalServiceHandler.new)
-  mount service, at: service.full_name
+  namespace :rpc do
+    service = Demo::V1::LocationRetrievalService.new(LocationRetrievalServiceHandler.new)
+    mount service, at: service.full_name
+  end
 end
