d96a923a1df047165e0498a20b381d15a018705a
Anthony Bobsin <anthony.bobsin.dev@gmail.com>
2019-11-20 19:08:46 -0500

    LocationRetrieval service

---
+++ app/handlers/location_retrieval_service_handler.rb
@@ -0,0 +1,5 @@
+class LocationRetrievalServiceHandler
+  def get_locations(req, env)
+    { locations: [{ id: 1, warehouse_id: 1, code: 'fake', address: '123 main st.' }] }
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
+require 'rpc/demo/v1/locations_twirp'
+require 'rpc/demo/v1/locations_pb'
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  service = Demo::V1::LocationRetrievalService.new(LocationRetrievalServiceHandler.new)
+  mount service, at: service.full_name
 end
