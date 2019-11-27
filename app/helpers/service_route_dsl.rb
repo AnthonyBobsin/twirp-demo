7e2b4da11805af67e4ab6a519d0864f724596e2c
Anthony Bobsin <anthony.bobsin.dev@gmail.com>
2019-11-27 00:03:06 -0500

    remove byebu

--- app/helpers/service_route_dsl.rb
+++ app/helpers/service_route_dsl.rb
@@ -1,7 +1,6 @@
 module ServiceRouteDsl
   def mount_service(service_klass, handler_klass)
     srv = TwirpServiceBuilder.build(service_klass, handler_klass)
-    require 'byebug'; byebug
     mount srv, at: srv.full_name
   end
 end
